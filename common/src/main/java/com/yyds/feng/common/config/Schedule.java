d479d363c0ddc1a79fd7f7062fe4ba1b8691bba6
lidengfeng@tsingcloud.com <lidengfeng@tsingcloud.com>
2022-07-13 16:18:04 +0800

    定时任务

---
+++ common/src/main/java/com/yyds/feng/common/config/Schedule.java
@@ -0,0 +1,15 @@
+package com.yyds.feng.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @Desc
+ * @Author Lee
+ * @Date 2022/7/13 16:15
+ */
+
+@Configuration
+@EnableScheduling
+public class Schedule {
+}
--- tgball/src/main/java/com/yyds/feng/tgball/component/SaticScheduleTask.java
+++ tgball/src/main/java/com/yyds/feng/tgball/component/SaticScheduleTask.java
@@ -6,14 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.io.UnsupportedEncodingException;
 
-@Configuration      //1.主要用于标记配置类，兼备Component的效果。
-@EnableScheduling
+@Component
 public class SaticScheduleTask {
-//    @Scheduled(cron = "0/5 * * * * ?")
-    //或直接指定时间间隔，例如：5秒 5000
+
     @Autowired
     TgBall tgBall;
 
